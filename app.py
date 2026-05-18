055301327da4b945dc7d66b0127588e850137cc9
raparthiharshithroy-web <raparthiharshithroy@gmail.com>
2026-05-18 20:11:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -89,5 +89,5 @@ def get_stock(symbol):
         traceback.print_exc()
         return jsonify({"error": str(e)}), 500
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
